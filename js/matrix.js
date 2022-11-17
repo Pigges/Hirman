43420371965590b38db5024592b50961561d03a7
Pigges <pigges@tutanota.com>
2022-11-17 16:35:20 +0100

    adjusted the matrix to handle resizing

--- js/matrix.js
+++ js/matrix.js
@@ -1,36 +1,77 @@
-// Initialising the canvas
-const canvas = document.getElementById('matrix');
-const ctx = canvas.getContext('2d');
+function createCanvas(cWidth, cHeight) {
 
-// Setting the width and height of the canvas
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+  // Creates and setting up the canvas
+  let canvas = document.createElement('canvas');
+  
+  canvas.id             = 'matrix';
+  canvas.style.position = 'absolute';
+  canvas.style.left     = '0';
+  canvas.style.top      = '0';
+  canvas.style.zIndex   = '-100';
+  canvas.width          = cWidth;
+  canvas.height         = cHeight;
 
-// Setting up the letters
-var letters = '10';
-letters = letters.split('');
-
-// Setting up the columns
-const fontSize = 10,
-    columns = canvas.width / fontSize;
-
-// Setting up the drops
-let drops = [];
-for (let i = 0; i < columns; i++) {
-  drops[i] = 1;
+  document.body.setAttribute("oncontextmenu", "return false");
+  document.body.appendChild(canvas);
+  return canvas;
 }
+
+function init() {
+  // Define width and height for the canvas
+  const cWidth = window.innerWidth;
+  const cHeight = window.innerHeight;
+
+  // Create the canvas
+  const canvas = createCanvas(cWidth, cHeight);
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = "#000";
+  ctx.fillRect(0,0,cWidth,cHeight)
+  return canvas
+}
+
+let canvas = init();
+scaling();
+
+function scaling() {
+  // Setting up the letters
+  letters = '10';
+  canvas.letters = letters.split('');
+
+  // Setting up the columns
+  canvas.fontSize = 10;
+  canvas.columns = canvas.width / canvas.fontSize;
+
+  // Setting up the drops
+  canvas.drops = [];
+  for (let i = 0; i < canvas.columns; i++) {
+    canvas.drops[i] = canvas.height;
+  }
+}
+
+
+// Make sure the canvas adjusts to window resizing
+addEventListener("resize", (event) => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  scaling()
+});
+
+// Define the context
+const ctx = canvas.getContext('2d');
 
 // Setting up the draw function
 function draw() {
+  // Setting the color to black with a opacity of 10%
   ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+  // Paint a rectangle with the applyied color
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < drops.length; i++) {
+  for (var i = 0; i < canvas.drops.length; i++) {
     const text = letters[Math.floor(Math.random() * letters.length)];
     ctx.fillStyle = '#0f0';
-    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-    drops[i]++;
-    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
-      drops[i] = 0;
+    ctx.fillText(text, i * canvas.fontSize, canvas.drops[i] * canvas.fontSize);
+    canvas.drops[i]++;
+    if (canvas.drops[i] * canvas.fontSize > canvas.height && Math.random() > .95) {
+      canvas.drops[i] = 0;
     }
   }
 }
